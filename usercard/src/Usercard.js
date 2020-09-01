7e2466472715f7f8be4f8b47eccf88d840a8ee90
HenryRDavis <rainfallsprings@gmail.com>
2020-09-01 18:39:18 -0500

    spending too much on styling. animation is not pretty but I tried

--- usercard/src/Usercard.js
+++ usercard/src/Usercard.js
@@ -6,6 +6,7 @@ import styled from 'styled-components'
 const StyledDiv = styled.div`
 display:flex;
 flex-direction:column;
+justify-content:center;
 background-color:white;
 color:black;
 &:hover{
@@ -13,6 +14,25 @@ color:black;
 }
 h2:hover{
     color: coral
+}
+`
+
+const StyledImg = styled.div`
+    position:relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+&:hover{
+    transform: rotateY(180deg);
+}
+#av, #other{
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+}
+#other{
+    transform: rotateY(180deg);
 }
 `
 
@@ -39,7 +59,10 @@ class Usercard extends React.Component {
             <div>{
                 this.state.userProfiles.map((item) => ( 
                     <div className='gitCard'> 
-                        <img src={item.avatar_url} alt={`photo of ${item.login}`}/>
+                        <StyledImg>
+                        <img id='av'src={item.avatar_url} alt={`photo of ${item.login}`}/>
+                        <img id='other' src='https://media1.tenor.com/images/c9fb849bef199916891b147fe279bdc7/tenor.gif'/>
+                        </StyledImg>
                         <StyledDiv> 
                         <h2> Name: <br/>{item.login}</h2>
                         <h2> Bio: <br/>{item.bio}</h2>
